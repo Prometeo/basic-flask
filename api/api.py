cd0e8c54582b01c76ea700e9ae73b2d38c00644d
Prometeo <xairi.valdivia@gmail.com>
2019-10-24 22:21:29 -0500

    Simple response

--- api/api.py
+++ api/api.py
@@ -1,12 +1,18 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
+# Users dictionary
+users_dict = [{'id': 1, 'name': 'Kunka', 'age': 30},
+              {'id': 2, 'name': 'pudge', 'age': 25}]
+
+
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Title</h1><p>Paragraph.</p>"
+    return jsonify(users_dict)
 
 
 app.run()
